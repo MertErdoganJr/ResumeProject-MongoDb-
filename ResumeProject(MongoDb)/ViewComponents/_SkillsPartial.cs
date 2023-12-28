75dfb9e6346136b0e35b79ea1fbf3d9b737d2f43
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-29 02:57:35 +0300

    Add Admin Page

---
+++ ResumeProject(MongoDb)/ViewComponents/_SkillsPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProject_MongoDb_.ViewComponents
+{
+    public class _SkillsPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
---
+++ ResumeProject(MongoDb)/ViewComponents/_TestimonialPatial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ResumeProject_MongoDb_.ViewComponents
+{
+    public class _TestimonialPatial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
