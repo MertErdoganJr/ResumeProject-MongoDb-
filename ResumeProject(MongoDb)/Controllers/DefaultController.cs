a351b564e0873b8ce21598b00cf2663b82be4185
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-29 02:15:36 +0300

    Add Main Page ViewComponents

---
+++ ResumeProject(MongoDb)/Controllers/DefaultController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using ResumeProject_MongoDb_.DAL.Entities;
+using ResumeProject_MongoDb_.DAL.Settings;
+
+namespace ResumeProject_MongoDb_.Controllers
+{
+    public class DefaultController : Controller
+    {        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ResumeProject(MongoDb)/ViewComponents/_SpinnerPartial.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using ResumeProject_MongoDb_.DAL.Entities;
+using ResumeProject_MongoDb_.DAL.Settings;
+
+namespace ResumeProject_MongoDb_.ViewComponents
+{
+    public class _SpinnerPartial : ViewComponent
+    {
+        private readonly IMongoCollection<About> _aboutCollection;
+        public _SpinnerPartial(IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _aboutCollection = database.GetCollection<About>(_databaseSettings.AboutCollectionName);
+
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _aboutCollection.Find(x => true).ToList();
+            return View(values);
+        }
+    }
+}
