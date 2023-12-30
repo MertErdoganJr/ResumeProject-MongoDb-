d3a9b3815e409ac3a8f4ea26b29e97e86d334d47
Mert Erdoğan <104645872+MertErdoganJr@users.noreply.github.com>
2023-12-31 02:56:21 +0300

    Add Experince List and Add with Ajax

---
+++ ResumeProject(MongoDb)/Controllers/ExperienceController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using ResumeProject_MongoDb_.DAL.Entities;
+using ResumeProject_MongoDb_.DAL.Settings;
+
+namespace ResumeProject_MongoDb_.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        private readonly IMongoCollection<Experience> _experinceCollection;
+        public ExperienceController(IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _experinceCollection = database.GetCollection<Experience>(_databaseSettings.ExperienceCollectionName);
+        }
+        public IActionResult Index()
+        {          
+            return View();
+        }
+
+
+        public async Task<IActionResult> ExperienceList()
+        {
+            var values = await _experinceCollection.Find(x => true).ToListAsync();
+            var jsonExperince = JsonConvert.SerializeObject(values);
+            return Json(jsonExperince);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExperience(Experience experience)
+        {
+            await _experinceCollection.InsertOneAsync(experience);
+            var values = JsonConvert.SerializeObject(experience);
+            return Json(values);
+
+        }
+    }
+}
